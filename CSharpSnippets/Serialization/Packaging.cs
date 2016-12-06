cd571de1bf5f3f0695338fa37b5361ed0b1f42d6
Warwick <uozu.aho@gmail.com>
2016-12-06 01:52:06 -0800

    clean up Serialization/Packaging

--- CSharpSnippets/Serialization/Packaging.cs
+++ CSharpSnippets/Serialization/Packaging.cs
@@ -10,24 +10,14 @@ namespace CSharpSnippets.Serialization
         /// </summary>
         public static void PackagingExample()
         {
-            var a = new A
-            {
-                Name = "A_name",
-                MyB = new B { Name = "B name" },
-                MyC = new C
-                {
-                    Name = "C name",
-                    LastName = "C lastname"
-                },
-            };
-            a.MyBRef = a.MyB;
+            var testObj = TestObj.GetDefault();
 
             var packPath = @"C:\temp\blahpack.pack";
             using (var fileStream = new FileStream(packPath, FileMode.Create))
             {
                 using (var packer = new ClassDataPacker(fileStream))
                 {
-                    packer.WriteEntity(a, x => x.Name);
+                    packer.WriteEntity(testObj, x => x.Name);
                 }
             }
 
@@ -37,27 +27,9 @@ namespace CSharpSnippets.Serialization
                 {
                     Console.WriteLine(part.Uri);
                 }
-                var deserializedA = pack.GetEntity<A>("A_name");
-                Console.WriteLine("Deserialized A: " + deserializedA);
+                var deserializedObj = pack.GetEntity<TestObj>("A_name");
+                Console.WriteLine("Deserialized TestObj: " + deserializedObj);
             }
-        }
-
-        public class A
-        {
-            public string Name { get; set; }
-            public B MyB { get; set; }
-            public C MyC { get; set; }
-            public B MyBRef { get; set; }
-        }
-
-        public class B
-        {
-            public string Name { get; set; }
-        }
-
-        public class C : B
-        {
-            public string LastName { get; set; }
         }
     }
 }
